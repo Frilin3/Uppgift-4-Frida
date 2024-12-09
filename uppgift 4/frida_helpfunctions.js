011410195039fcb0001d0e4656dae3091b72afe5
Frilin3 <frida.lindkvist@gritacademy.se>
2024-12-09 21:22:34 +0100

    Add files via upload

---
+++ uppgift 4/frida_helpfunctions.js
@@ -0,0 +1,8 @@
+
+charRemove( 'a', "safari" ) 
+function charRemove( char , text) {
+    text = text.replaceAll (char, "")
+    console.log( text  )
+    
+}
+
---
+++ uppgift 4/script.js
@@ -0,0 +1,9 @@
+let cat = {
+    alive : true ,
+    name : "Gustav"
+}
+
+function catButton(){
+    alert("this cats name is " + cat.name + " and it is alive")
+}
+
